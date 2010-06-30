ed11672354f4870bbd11c24773999db3823178ae
Jason R. Coombs <jaraco@jaraco.com>
2010-06-30 11:11:35 -0400

    Bumped to 0.3.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 	name="openpack",
 	author="Christian Wyglendowski (YouGov)",
-	version="0.3.1",
+	version="0.3.2",
 	packages=['openpack'],
 	install_requires=[
 		'lxml',
