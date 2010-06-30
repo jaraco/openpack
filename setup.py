bf38faa32cfb1c96ee2a0a8dea2b52ce4379c0f8
Jason R. Coombs <jaraco@jaraco.com>
2010-06-30 14:33:51 -0400

    Bumping revision to 0.3.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 	name="openpack",
 	author="Christian Wyglendowski (YouGov)",
-	version="0.3.2",
+	version="0.3.3",
 	packages=['openpack'],
 	install_requires=[
 		'lxml',
