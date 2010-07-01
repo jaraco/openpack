9af6dd5d2a2e597b6b12696c3bf626c092d4684a
Jason R. Coombs <jaraco@jaraco.com>
2010-07-01 12:03:18 -0400

    Updated tag to 'dev'

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 	name="openpack",
 	author="Christian Wyglendowski (YouGov)",
-	version="0.3.3",
+	version="0.3.4-dev",
 	packages=['openpack'],
 	install_requires=[
 		'lxml',
