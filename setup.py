7c915b32a1ead89abf6e7af38220b8e2a02df9e5
Jason R. Coombs <jason.coombs@yougov.com>
2010-07-02 11:38:34 -0700

    Bumping to 0.4 as these recent checkins have substantially changed the API

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 	name="openpack",
 	author="Christian Wyglendowski (YouGov)",
-	version="0.3.4-dev",
+	version="0.4-dev",
 	packages=['openpack'],
 	install_requires=[
 		'lxml',
