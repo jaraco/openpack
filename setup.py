ae65b0fcd8830ed8c5e8088aac4dc15210b24ad9
Jason R. Coombs <jaraco@jaraco.com>
2011-09-23 14:02:59 -0400

    Adding required metadata

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ with open(os.path.join(os.path.dirname(__file__), 'README')) as f:
 setup_params = dict(
 	name="openpack",
 	author="Christian Wyglendowski (YouGov), Jason R. Coombs (YouGov)",
+	author_email="open.source@yougov.com",
+	url="https://bitbucket.org/yougov/openpack",
 	use_hg_version=True,
 	long_description=long_description,
 	packages=['openpack'],
@@ -22,7 +24,7 @@ setup_params = dict(
 			],
 	},
 	setup_requires = [
-		'hgtools >= 0.4.7',
+		'hgtools>=1.0',
 	],
 )
 
