193e6d88a55e6ec62662e790c166b202e1c06aae
Jason R. Coombs <jason.coombs@yougov.com>
2017-11-12 13:12:24 -0500

    Fix module runner invocation

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,3 +1,8 @@
+2.1.1
+~~~~~
+
+Fixed module runner invocations.
+
 2.1
 ~~~
 
--- openpack/zip-listdir.py
+++ openpack/zip-listdir.py
@@ -1,4 +1,4 @@
 import openpack.editor
 
 
-__name__ == '__main__' and openpack.editor.pack_dir_cmd
+__name__ == '__main__' and openpack.editor.pack_dir_cmd()
