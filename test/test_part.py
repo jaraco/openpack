a3995eb4fb7b1831583ac4683a94d75612206ae5
Jason R. Coombs <jaraco@jaraco.com>
2020-11-07 11:44:56 -0500

    Move part tests to test suite so they're run and fix broken tests.

--- test/test_part.py
+++ test/test_part.py
@@ -8,8 +8,8 @@ class TestParts:
         cls.package = Package()
 
     def test_create(self):
-        self.part = Part(self.package, '/something.html', 'text/html')
+        self.part = Part(self.package, '/something.html', content_type='text/html')
 
     def test_bad_names(self):
         with pytest.raises(ValueError):
-            Part(self.package, 'something', 'text/html')
+            Part(self.package, 'something')
