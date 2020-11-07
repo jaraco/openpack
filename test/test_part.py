ffc1bb8689a2a435080dcc7361e6f4bf66b3b943
Jason R. Coombs <jaraco@jaraco.com>
2020-11-07 11:46:56 -0500

    No need to have a fixture for a throw-away package.

--- test/test_part.py
+++ test/test_part.py
@@ -4,12 +4,9 @@ from openpack.basepack import Package, Part
 
 
 class TestParts:
-    def setup_class(cls):
-        cls.package = Package()
-
     def test_create(self):
-        self.part = Part(self.package, '/something.html', content_type='text/html')
+        self.part = Part(Package(), '/something.html', content_type='text/html')
 
     def test_bad_names(self):
         with pytest.raises(ValueError):
-            Part(self.package, 'something')
+            Part(Package(), 'something')
